7fc0bb0aa7986d6ac6e3506b4a424942db1a5ddc
Salavat <salavatfederal@gmail.com>
2023-04-12 14:08:21 +0300

    fixed1

---
+++ src/main/Date.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Date {
+    public static String generateDate(int days, String pattern) {
+        return LocalDate.now()
+                .plusDays(days)
+                .format(DateTimeFormatter
+                        .ofPattern(pattern));
+    }
+
+}
